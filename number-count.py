6f89f01c879620604234f76df32eb4676d793cb8
rkyger <rkyger@protonmail.com>
2019-10-08 17:25:40 -0700

    Added hw files.

--- number-count.py
+++ number-count.py
@@ -4,7 +4,9 @@
 
 # This program counts in the number of 2s in the string below.
 
+# string of numbers
 numbers = "112345678911234566"
 
+#print number of twos
 print("There are", numbers.count("2") , "twos.")
 
