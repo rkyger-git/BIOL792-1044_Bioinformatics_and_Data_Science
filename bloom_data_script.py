7d3b3c08cdb63f51d7a50b36ef68bf269b9b7796
rkyger <rkyger@protonmail.com>
2019-10-22 17:19:17 -0700

    fixed comment typo

--- bloom_data_script.py
+++ bloom_data_script.py
@@ -2,8 +2,7 @@
 
 #log_body_size.py
 
-#This script reads through "Bloom_etal_2018_Reduced_Dataset.csv" and prints out the taxon name, the diadromous status, and the total log body size for all the lines in the file. 
-
+#This script reads through "Bloom_etal_2018_Reduced_Dataset.csv" and prints out the taxon name, the diadromous status, and the total log body size. 
 #define the file of interest
 InFileName = "Bloom_etal_2018_Reduced_Dataset.csv"
 
